f1fd3dcfc98cdde37a8de0af52df1bfff1f24f17
Josh Haws <joshhaws0@gmail.com>
2019-01-03 01:09:06 -0600

    update

--- src/App.js
+++ src/App.js
@@ -6,20 +6,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to josh.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div>Josh Haws | Full Stack Dev</div>
+        <div>Comning soon...</div>
       </div>
     );
   }
