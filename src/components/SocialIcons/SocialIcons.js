e631f91defacf20a66b0d2371ee404c008b8fafb
Josh Haws <joshhaws0@gmail.com>
2019-01-30 23:38:00 -0600

    added resume

--- src/components/SocialIcons/SocialIcons.js
+++ src/components/SocialIcons/SocialIcons.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import pdf from '../../images/resume.pdf';
 
 import './style.scss';
 
@@ -8,10 +10,11 @@ const SocialIcons = (props, context) => {
   
   return (
     <div className="social-icons animate-icons">
-        <a target="_blank" rel="noopener noreferrer" href="https://github.com/joshhaws" style={ { color: colorPrimary } }><i className="fab fa-github"></i></a>
-        <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/joshhaws" style={ { color: colorPrimary } }><i className="fab fa-linkedin"></i></a>
-        <a target="_blank" rel="noopener noreferrer" href="mailto:joshhaws0@gmail.com?Subject=Hello" target="_top" style={ { color: colorPrimary } }><i className="fa fa-envelope"></i></a>
-      </div>
+      <a target="_blank" rel="noopener noreferrer" href="https://github.com/joshhaws" style={ { color: colorPrimary } }><i className="fab fa-github"></i></a>
+      <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/joshhaws" style={ { color: colorPrimary } }><i className="fab fa-linkedin"></i></a>
+      <a target="_blank" rel="noopener noreferrer" href="mailto:joshhaws0@gmail.com?Subject=Hello" target="_top" style={ { color: colorPrimary } }><i className="fa fa-envelope"></i></a>
+      <a href={pdf} style={ { color: colorPrimary } }><i className="fa fa-file"></i></a>
+    </div>
   );
 };
 
